78ed8bff6355bfa36155e7116f72143d419af856
Arignan <arignan03@gmail.com>
2025-02-28 00:30:58 +0530

    home spinning reverse added

--- script.js
+++ script.js
@@ -138,22 +138,25 @@ function scroolToTop(){
   window.scrollTo(0,0);
 }
 
+//funtion for profile spins effect while hovering
 document.addEventListener('DOMContentLoaded', function() {
   const picContainers = document.querySelectorAll('.section__pic-container');
   const cslider = document.querySelector('.cslider');
   const defaultSpeed = '20s'; // Default speed when not hovering
-  const hoverSpeed = '3s';     // Faster speed on hover
+  const hoverSpeed = '20s';     // Faster speed on hover
 
   picContainers.forEach(container => {
     container.addEventListener('mouseover', function() {
       if (cslider) {
         cslider.style.animationDuration = hoverSpeed;
+        cslider.style.animationDirection = 'reverse'; /* Reverse direction on hover */
       }
     });
 
     container.addEventListener('mouseout', function() {
       if (cslider) {
         cslider.style.animationDuration = defaultSpeed;
+        cslider.style.animationDirection = 'normal'; /* Normal direction when not hovering */
       }
     });
   });
